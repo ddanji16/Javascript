f0d40dfc75340064ca16cec5632cbc95289c6463
Daniel Anthony Dela Cruz <danskidelacruz@gmail.com>
2025-07-10 03:04:56 +0800

    js files

---
+++ index.js
@@ -0,0 +1,77 @@
+
+function btun(obj) {
+    obj.style.color = "red";
+    obj.innerHTML = "sure?"
+
+
+
+}
+
+function btunn(ob) {
+    ob.style.color = "black";
+    ob.innerHTML = "yup"
+
+
+
+}
+function txt(){
+    document.getElementById('haha').innerHTML = "miss kaba?"
+    document.getElementById('boton').style.visibility = "hidden";
+    document.getElementById('heading').innerHTML = '<h2> aray ko</h2>'
+}
+
+
+alert("loaded");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+document.getElementById('btn2').style.visibility = "hidden"
+
+function txtone() {
+
+document.getElementById('text').innerHTML = "Hello"
+document.getElementById('btn1').style.visibility = "hidden"
+document.getElementById('btn2').style.visibility = "visible"
+
+}
+function txttwo() {
+
+    document.getElementById('text').innerHTML = "World"
+    document.getElementById('btn2').style.visibility = "hidden"
+    document.getElementById('btn1').style.visibility = "visible"
+
+    }
+
+
+function text(obj){
+    obj.innerHTML = "hi"
+    obj.style.color = "red"
+
+}
+*/
