dc68a23776045a2d70797fba0899b33cacfcff7b
Daniel Anhtony Dela Cruz <anthonydelac1602@gmail.com>
2026-01-11 23:54:54 +0800

    added new codes like JSON

--- script.js
+++ script.js
@@ -1,87 +1,84 @@
-alert('yo');
+alert("yo");
 console.log("hello world");
 
+//JavaScript Functions
 
-    //JavaScript Functions
+function remove() {
+  let remove = document.getElementById("box1");
+  remove.style.display = "none";
 
-    function remove(){
-        let remove = document.getElementById('box1');
-        remove.style.display = 'none'
-
-        /*
+  /*
         remove.style.backgroundColor = 'Red';
         */
-    }
+}
 
+// Javascript While Loops
 
+let i = 0;
 
+while (i < 210) {
+  console.log(i);
+  i++;
 
-    // Javascript While Loops
+  //if the i == to 10 then the loop will break or stop even the condition is true
 
-        let i = 0;
+  if (i == 10) {
+    console.log("break");
+    break;
+  }
+}
 
-        while(i<210){
-            console.log(i);
-            i++;
+//JavaScript Array with While Loops
+// Iterate Array using while Loops
 
-         //if the i == to 10 then the loop will break or stop even the condition is true   
+let fruits = ["apple", "mango", "banana", "orange", "grapes"];
+let j = 0;
 
-        if(i == 10){
-            console.log("break");
-            break;
-        }
-        }
-
-
-
-        //JavaScript Array with While Loops
-        // Iterate Array using while Loops
-
-
-        let fruits = ['apple','mango','banana','orange','grapes'];
-        let j = 0;
-
-        /*
+/*
         let j = fruits.length-1;
         */
 
-        while(fruits[j]){
-            console.log(fruits[j]);
-            j++;
-            /*
+while (fruits[j]) {
+  console.log(fruits[j]);
+  j++;
+  /*
             j--;
             */
-        }
+}
+
+//DO WHILE LOOP IN JAVASCRIPS
+
+let g = 0;
+
+do {
+  console.log(g);
+  g++;
+} while (g < 4);
+
+// loop challenge add all numbers using loopp
+
+let num = [70, 60, 50, 10, 40, 30, 20];
+let k = 0;
+let sum = 0;
+
+while (num[k]) {
+  sum += num[k];
+  k++;
+}
+console.log(sum);
 
 
 
-            //DO WHILE LOOP IN JAVASCRIPS
-
-        let g = 0;
-
-        do{
-            console.log(g);
-            g++;
-        }while(g<4);
 
 
 
 
-        // loop challenge add all numbers using loopp
+ //JSON FORTMAT
 
 
-        let num = [70,60,50,10,40,30,20];
-        let k = 0;
-        let sum = 0;
-       
+ let cars = {
+  brand: "toyota",
+  price: 1000
 
-        while(num[k]){
-            sum += num[k];
-             k++;  
-          
-
-        }
-        console.log(sum);
-        
-
-        
+ }
+ console.log(cars);
